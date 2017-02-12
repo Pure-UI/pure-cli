c134ac3ca2d1d5e86053bb47ab196f0bf597b5c9
mo <feng_zilong@163.com>
2017-02-13 00:46:14 +0800

    tweaks

--- bin/pure.js
+++ bin/pure.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 'use strict';
 
+const yargs = require( 'yargs' );
 const build = require( '../lib/build' );
 const watch = require( '../lib/watch' );
 
-build();
+const argv = yargs
+	.alias( 'w', 'watch' )
+	.argv;
+
+if ( argv.watch ) {
+	watch();
+} else {
+	build( {
+		entry: argv.entry,
+		name: argv.name,
+	} );
+}
