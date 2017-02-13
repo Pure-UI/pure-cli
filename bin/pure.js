565f32dc68ed27f576e9c58335122eddff998203
mo <feng_zilong@163.com>
2017-02-14 02:02:00 +0800

    Add --bundle option

--- bin/pure.js
+++ bin/pure.js
@@ -7,12 +7,20 @@ const watch = require( '../lib/watch' );
 
 const argv = yargs
 	.alias( 'w', 'watch' )
+	.boolean( 'external' )
 	.argv;
 
 if ( argv.watch ) {
 	watch();
+} else if ( argv.bundle ) {
+	build( {
+		bundle: true,
+		entry: argv.entry,
+		name: argv.name,
+	} );
 } else {
 	build( {
+		bundle: false,
 		entry: argv.entry,
 		name: argv.name,
 	} );
